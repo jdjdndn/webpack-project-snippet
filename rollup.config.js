9c97d3cf41e45aa50d6ba3948ee0b78021b769c8
yucheng <2667389861@qq.com>
2023-06-01 17:41:39 +0800

    收集项目下所有export变量，生成snippet

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+
+module.exports =  {
+  input: "lib/index.js",
+  output: {
+    dir: "dist",
+    // file: "index.js",
+    format: "umd",
+    name: "project-sniappet",
+  },
+};
---
+++ utils/index.js
@@ -0,0 +1,46 @@
+/**
+ * 获取资源真实路径
+ * @param {*} resource 资源路径
+ * @returns 资源真实路径
+ */
+function getRealResource(resource) {
+  try {
+    resource = decodeURIComponent(resource)
+  } catch (error) {
+  }
+  let index = resource.indexOf('?')
+  if (index !== -1) {
+    resource = resource.slice(0, index)
+  }
+
+  index = resource.indexOf('#')
+  if (index !== -1) {
+    resource = resource.slice(0, index)
+  }
+  return resource
+}
+
+/**
+ * 根据数组变成vscode可用的待选数组
+ * @param {*} list
+ * @param {*} prefix
+ * @returns
+ */
+function getChooseList(list, prefix = "") {
+  let arr = list;
+  if (prefix) {
+    arr = list.filter(Boolean).map((it) => {
+      if (it.match(/\s+/)) {
+        return it;
+      } else {
+        return prefix + it;
+      }
+    });
+  }
+  return "${1|" + arr.join(",") + "|}";
+}
+
+module.exports = {
+  getRealResource,
+  getChooseList
+}
